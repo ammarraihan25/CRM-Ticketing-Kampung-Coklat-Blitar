03953e4dcb628cf9749d0c664a944ec3cc933a6f
devannoap31 <devannoap318@gmail.com>
2026-08-18 12:40:03 +0700

    first commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'kampung_coklat',
+      autoLoadEntities: true,
+      synchronize: true, // Not recommended for production
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
